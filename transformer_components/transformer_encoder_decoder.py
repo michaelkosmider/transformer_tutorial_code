03b573918b057b5694dc63d3767aa8cdc9288406
Michael Kosmider <michael.kosmider24@gmail.com>
2025-07-09 21:21:25 -0400

    tmp

--- transformer_components/transformer_encoder_decoder.py
+++ transformer_components/transformer_encoder_decoder.py
@@ -24,20 +24,18 @@ class TransformerEncoderDecoder(nn.Module):
         return logits
 
     def generate(
-        self, X_src, src_key_padding_mask, beam_K, beam_max_len, SOS_IDX, PAD_IDX
+        self, X_src, src_key_padding_mask, num_beams, max_beam_len, SOS_IDX, PAD_IDX
     ):
-
-        device = X_src.device
 
         # Get source features (encoder output). Only need to do this once.
         src_features = self.encoder(X_src, src_key_padding_mask)
 
         # Initialize empty beams with SOS + PAD
         beams = torch.full(
-            (beam_K, beam_max_len), PAD_IDX, dtype=torch.int, device=device
+            (num_beams, max_beam_len), PAD_IDX, dtype=torch.int, device=X_src.device
         )
         beams[:, 0] = SOS_IDX
-        total_causal_mask = get_causal_mask(beam_max_len, device=device)
+        total_causal_mask = get_causal_mask(max_beam_len, device=X_src.device)
 
         # Obtain logits for a single SOS token, which will attend to the source sentence.
         decoder_in = beams[:1, :1]
@@ -51,19 +49,15 @@ class TransformerEncoderDecoder(nn.Module):
             src_key_padding_mask,
         )
 
-        # Obtain the score for each token. Also obtain a probability distribution over all tokens.
-        first_token_probs = torch.softmax(logits, dim=-1).view(-1)
-        first_token_scores = torch.log_softmax(logits, dim=-1).view(
-            -1
-        )  # Despite already having softmax, log_softmax is more numerically stable, and thus preferred.
+        first_token_scores = torch.log_softmax(logits[0], dim=-1).view(-1)
 
         # Sample K tokens, and place them in the beams. Initialize scores for each beam.
-        tokens = torch.multinomial(first_token_probs, beam_K)
+        beam_scores, tokens = torch.topk(first_token_scores, num_beams)
         beams[:, 1] = tokens
-        beam_scores = first_token_scores[tokens].view(beam_K, 1)
+        beam_scores = beam_scores.view(num_beams, 1)
 
         # Deterministic beam search for remaining tokens.
-        for beam_cur_len in range(2, beam_max_len):
+        for beam_cur_len in range(2, max_beam_len):
 
             # Get logits for the next token (across all beams).
             decoder_in = beams[:, :beam_cur_len]
@@ -79,8 +73,8 @@ class TransformerEncoderDecoder(nn.Module):
             next_token_logits = logits[:, beam_cur_len - 1, :]
 
             # Select the top K beams from the pool of candidates, update scores.
-            candidates = beam_scores + next_token_logits
-            scores, indices = torch.topk(candidates.view(-1), beam_K)
+            candidates = beam_scores + torch.log_softmax(next_token_logits, dim=-1)
+            scores, indices = torch.topk(candidates.view(-1), num_beams)
             beam_indices = indices // self.decoder.vocab_size
             tokens = indices % self.decoder.vocab_size
 
@@ -88,6 +82,75 @@ class TransformerEncoderDecoder(nn.Module):
                 beam_indices
             ]  # Re-arrange the beams in terms of score. Some beams are eliminated, while some are duplicated one or more times.
             beams[:, beam_cur_len] = tokens
-            beam_scores = beam_scores[beam_indices] + scores.view(beam_K, 1)
+            beam_scores = scores.view(num_beams, 1)
 
         return beams[0]
+
+    # def generate(
+    #     self, X_src, src_key_padding_mask, beam_K, beam_max_len, SOS_IDX, PAD_IDX
+    # ):
+
+    #     device = X_src.device
+
+    #     # Get source features (encoder output). Only need to do this once.
+    #     src_features = self.encoder(X_src, src_key_padding_mask)
+
+    #     # Initialize empty beams with SOS + PAD
+    #     beams = torch.full(
+    #         (beam_K, beam_max_len), PAD_IDX, dtype=torch.int, device=device
+    #     )
+    #     beams[:, 0] = SOS_IDX
+    #     total_causal_mask = get_causal_mask(beam_max_len, device=device)
+
+    #     # Obtain logits for a single SOS token, which will attend to the source sentence.
+    #     decoder_in = beams[:1, :1]
+    #     tgt_mask = total_causal_mask[:1, :1]
+    #     tgt_key_padding_mask = decoder_in == PAD_IDX
+    #     logits = self.decoder(
+    #         decoder_in,
+    #         src_features,
+    #         tgt_mask,
+    #         tgt_key_padding_mask,
+    #         src_key_padding_mask,
+    #     )
+
+    #     # Obtain the score for each token. Also obtain a probability distribution over all tokens.
+    #     first_token_probs = torch.softmax(logits, dim=-1).view(-1)
+    #     first_token_scores = torch.log_softmax(logits, dim=-1).view(
+    #         -1
+    #     )  # Despite already having softmax, log_softmax is more numerically stable, and thus preferred.
+
+    #     # Sample K tokens, and place them in the beams. Initialize scores for each beam.
+    #     tokens = torch.multinomial(first_token_probs, beam_K)
+    #     beams[:, 1] = tokens
+    #     beam_scores = first_token_scores[tokens].view(beam_K, 1)
+
+    #     # Deterministic beam search for remaining tokens.
+    #     for beam_cur_len in range(2, beam_max_len):
+
+    #         # Get logits for the next token (across all beams).
+    #         decoder_in = beams[:, :beam_cur_len]
+    #         tgt_mask = total_causal_mask[:beam_cur_len, :beam_cur_len]
+    #         tgt_key_padding_mask = decoder_in == PAD_IDX
+    #         logits = self.decoder(
+    #             decoder_in,
+    #             src_features,
+    #             tgt_mask,
+    #             tgt_key_padding_mask,
+    #             src_key_padding_mask,
+    #         )
+    #         next_token_logits = logits[:, beam_cur_len - 1, :]
+
+    #         # Select the top K beams from the pool of candidates, update scores.
+    #         candidates = beam_scores + next_token_logits
+    #         scores, indices = torch.topk(candidates.view(-1), beam_K)
+    #         beam_indices = indices // self.decoder.vocab_size
+    #         tokens = indices % self.decoder.vocab_size
+
+    #         beams = beams[
+    #             beam_indices
+    #         ]  # Re-arrange the beams in terms of score. Some beams are eliminated, while some are duplicated one or more times.
+    #         beams[:, beam_cur_len] = tokens
+    #         beam_scores = beam_scores[beam_indices] + scores.view(beam_K, 1)
+
+    #     return beams[0]
